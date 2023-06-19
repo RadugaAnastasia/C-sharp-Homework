02c9cf82932a1fdbcf2a22b7e86ca1390865c210
Anastasiia <martishka85@list.ru>
2023-06-19 11:44:11 +0300

    add task 21 - Homework

---
+++ task19/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+//  и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine()!);
+    return number;
+}
+
+int ReverseNumber(int n)
+{
+    int k = 0;
+    while (n > 0)
+    {
+        k = 10*n + n % 10;
+        n /= 10; //n = n / 10
+    }
+    return k;
+}
+
+int N = Prompt("Введите пятизначное число: ");
+
+if(N >= 10000 && N < 100000)
+{
+    if(N == ReverseNumber(N))
+    {
+        Console.WriteLine($" {N} -> да");
+    }
+    else
+    {
+        Console.WriteLine($" {N} -> нет");
+    }
+}
+else
+{
+    Console.WriteLine("Введенное число не является пятизначным!");
+}
---
+++ task21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+//  и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+void Rasst(int xa, int ya, int za, int xb, int yb, int zb)
+{
+    double AB = Math.Sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya)+(zb-za)*(zb-za));
+    Console.WriteLine(AB);
+}
+
+int XA = Prompt("Введите координату точки xa: ");
+int YA = Prompt("Введите координату точки ya: ");
+int ZA = Prompt("Введите координату точки za: ");
+int XB = Prompt("Введите координату точки xb: ");
+int YB = Prompt("Введите координату точки yb: ");
+int ZB = Prompt("Введите координату точки zb: ");
+Rasst (XA, YA, ZA, XB, YB, ZB);
