108a51be57ff7e642489982beb2a2942c10e1508
Anastasiia <martishka85@list.ru>
2023-06-19 11:48:25 +0300

    add task 23 - Homework

---
+++ task23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+void FindThird(int n)
+{
+    int i = 1;
+    while(i <= n)
+{
+    Console.Write(i*i*i + "  ");
+    i ++;
+}
+}
+
+int N = Prompt("Введите число N: ");
+FindThird(N);
