86a828a12a67842a3c1d0c2a2255ee1d242247db
Anastasiia <martishka85@list.ru>
2023-07-05 22:36:25 +0300

    add task52

---
+++ task52/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 52
+// Задайте двумерный массив из целых чисел. Найдите среднее 
+// арифметическое элементов в каждом столбце. Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+int rows = Prompt("Введите количество строк: ");
+int columns = Prompt("Введите количество столбцов: ");
+int minValue = Prompt("Введите число для минимального диапазона значений: ");
+int maxValue = Prompt("Введите число для максимального диапазона значений: ");
+int[,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+FindAverage(array);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+void FindAverage(int[,] arr)
+{
+    double sum = 0;
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum += arr[i,j];
+        }
+        sum = sum / arr.GetLength(0);
+        Console.Write($"{sum};  ");
+        sum = 0;
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++) //GetLength(0) - длина строки
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) //GetLength(1) - длина столбцов
+        {
+            Console.Write(array[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
