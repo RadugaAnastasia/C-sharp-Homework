c5abb9381f01b481e9c90950e2b4227ef7f09fb4
Anastasiia <martishka85@list.ru>
2023-07-05 20:56:12 +0300

    add task47

--- task43/Program.cs
+++ task43/Program.cs
@@ -20,3 +20,27 @@ x = Math.Round(x, 3);
 y = Math.Round(y, 3);
 
 Console.WriteLine($" b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y}) ");
+
+
+//Решение от Эльвиры
+Console.Clear();
+Console.Write("Введите через пробел b1, k1, b2, k2: ");
+string[] f = Console.ReadLine().Split(); //строковый массив через пробел
+double b1 = double.Parse(f[0]); //вытаскиваем поэлементно из строкового массива и преобразовываем
+double k1 = double.Parse(f[1]);
+double b2 = double.Parse(f[2]);
+double k2 = double.Parse(f[3]);
+// GetPoint(b1, k1, b2, k2);
+Console.WriteLine(String.Join(" ", GetPoint(b1, k1, b2, k2))); //метод Join выводит в строку поэлементно через пробел на консоль
+
+double[] GetPoint(double inB1, double inK1, double inB2, double inK2)
+{
+    double[] result = new double[2];
+    result[0] = (inB1 - inB2) / (inK1 - inK2);
+    result[1] = inK1 * result[0] + inB1;
+    // for (int i = 0; i < result.length; i++)
+    // {
+    //     Console.Write($"{result[i]}");
+    // }
+    return result;
+}
---
+++ task47/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 47
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int rows = Prompt("Введите количество строк: ");
+int columns = Prompt("Введите количество столбцов: ");
+int minValue = Prompt("Введите число для минимального диапазона значений: ");
+int maxValue = Prompt("Введите число для максимального диапазона значений: ");
+
+double[,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+double[,] GetArray(int m, int n, int min, int max)
+{
+    double[,] result = new double[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().NextDouble()*(maxValue - minValue) + minValue;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++) //GetLength(0) - длина строки
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) //GetLength(1) - длина столбцов
+        {
+            Console.Write($"{array[i,j]:F4}  ");
+        }
+        Console.WriteLine();
+    }
+}
